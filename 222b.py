92243a2eddc3162c280a6e74d9a1364e8e3f952f
Taro Serigano <taroserigano@gmail.com>
2022-11-03 07:21:37 -0500

    Create 222b.py

---
+++ 222b.py
@@ -0,0 +1,88 @@
+2-2 Activity: Technology and Event Introduction
+IDS-403- Technology and Society 22EW1
+September 11th, 2022
+Integrate reliable evidence from varied sources
+I chose the great technology giant company, Microsoft for my technology of choice due
+to its popularity in the technology industry to write about in this project. Microsoft is being
+named a technology giant that helps a lot of businesses, beginners, and some other large
+enterprises to deploy their projects. I picked this technology because it’s the same one that I
+wrote about in my first week's activity for this class. I’ve been using computers for the past
+twenty years in counting which I went to school for, I can really say that I still don’t know a lot
+about them. I can take them apart and describe mainly very single components and some of the
+processes from the time your press the power button. There’s much more than gathering the parts
+together to connect them to the motherboard. A piece of Hardware is useless without software
+and the same for software too. They need a place to live, like parasites that require a living host
+to stay alive. Throughout this project I will be using this site: https://www.history.com/this-dayin-
+history/microsoft-founded to work on the project to provide supporting details, and
+https://www.microsoft.com/en-us/research/blog/functional-lens-promises-to-improve-thequality-
+of-life-for-diabetics-and-others/ for some more other supporting details as well.
+Describe a social, cultural, or global event
+This study source was downloaded by 100000795539880 from CourseHero.com on 11-03-2022 07:15:04 GMT -05:00
+https://www.coursehero.com/file/174893858/IDS-403-2-2-Activity-Technology-and-Event-Introductiondoxcdocx/
+The great technology giant company, Microsoft was founded on April 4th, 1975 two best
+friends name Bill Gates and Paul Allen. The two met while in high Scholl and decided that they
+will change the world. When Gates and Allen started the company Microsoft, it was originally
+called Micro-Soft since they were making microprocessors and software for an Altair 8800
+personal computer at that time. Allen then left his job as a programmer in Boston and Gates
+dropped out of Harvard University to focus more on their company. After publishing their Alter
+8800, by the end of 1978, the company’s sales went up to more than $1 million and they had to
+move the business headquarters to Bellevue, Washington. The organization then went for its
+licensing in MS-DOS to IBM for its first personal computer in 1981. Allen had to leave the
+company after finding out that he was diagnosed with Hodgkin’s lymphoma in 1983, but he got
+treated for the disease and ventured into other businesses.
+Describe the technology
+In the year of 1985, the company went to release a new operating system, Windows, with
+a graphical user interface (GUI) with a drop-down menu, scroll bars, and other features included.
+The company moved its headquarters to Redmond, Washington in the following year and also
+went public at $21 per share. The company revenue went up to $61 million. “In 1995, amidst
+skyrocketing purchases of personal computers for home and office use, Windows 95 made its
+debut. It included such innovations as the Start menu (TV commercials for Windows 95 featured
+the Rolling Stones singing “Start Me Up”) and 7 million copies of the new product were sold in
+the first five weeks. During the second half of the 1990s, Internet usage took off, and Microsoft
+introduced its web browser, Internet Explorer, in 1995.”
+Assess the societal or cultural influences
+This study source was downloaded by 100000795539880 from CourseHero.com on 11-03-2022 07:15:04 GMT -05:00
+https://www.coursehero.com/file/174893858/IDS-403-2-2-Activity-Technology-and-Event-Introductiondoxcdocx/
+This company has saved a lot of lives since it started. The owners have opened so many
+doors with opportunities for a lot of us, myself included. I remember when I had my first
+computer back in 1999, it was the greatest gift I ever had. I had an unmatching keyboard and
+mouse with a cream color. I treated it like it was gold that it was. Even though it wasn’t brand
+new, it was my computer and that’s when I started to type on a keyboard. I was eager to learn
+much more about computers and this is why I went to school for computers after high school,
+well after I attempted many other fields. This piece of technology will be with us for years to
+come. And all thanks to these two guys and so many others like the late Steve Job. We can save
+this topic for another project.
+Natural and applied sciences lens
+The natural and applied sciences lens that I know a lot of people can relate to is type 1
+diabetes. This is a really big issue in the American Culture and it’s not only affecting adults now,
+younger adults are being diagnosed with type 1 diabetes. NUI technology has the capability to
+provide benefits to users that are with type 1 diabetes and a better way to monitor their blood
+blue. When I used to work at the hospital and when I was doing labs as per my class curriculum,
+this is something I check on the patients throughout the time I was there. The small needles go
+through the patient's fingers, they have to rotate fingers at a time. The sticky sounds and pray that
+you’ll enough blood so that you won’t stick them again. Each time I will walk into the room,
+some of my patients’ favorite word was, “Again” with sad looks on their faces. With the help of
+NUI technology by Microsoft, patients won’t need to go through this as much “I’m used to
+testing myself six to eight times per day. I’ve been doing it for 30 years,” McFeely says. “But
+boy, when I think about my children… I mean, you have a spring-loaded needle that’s coming
+This study source was downloaded by 100000795539880 from CourseHero.com on 11-03-2022 07:15:04 GMT -05:00
+https://www.coursehero.com/file/174893858/IDS-403-2-2-Activity-Technology-and-Event-Introductiondoxcdocx/
+into your finger, and it hurts them. I can see their faces [when they test], and I can see them
+cringe.”
+Apply the social science lens to your technology
+Science and technology have become increasingly sophisticated which we as human
+being faced today. In today of social science in technology, we are in need of people with some
+understanding of social, political, economic, and legal issues as well as good understanding of
+technology. As stated by (Kathleen Vogel, 2014), we need to understand the tremendous
+technical problems and the controversies that we face.
+Reference
+History.com Editors. This day in History. Microsoft founded. (1975, April 4). Publisher A&E
+Television Networks. Retrieved September 3, 2022. Last Updated April 1, 2020. Original
+Published Date October 9, 2015 from https://www.history.com/this-day-in-history/microsoftfounded
+Microsoft Research Blog. Functional Lens Promises to Improve the Quality of Life for Diabetics
+and Others. Published July 27, 2011. Retrieved September 10th, 2022.
+https://www.microsoft.com/en-us/research/blog/functional-lens-promises-to-improve-thequality-
+of-life-for-diabetics-and-others/
+This study source was downloaded by 100000795539880 from CourseHero.com on 11-03-2022 07:15:04 GMT -05:00
+https://www.coursehero.com/file/174893858/IDS-403-2-2-Activity-Technology-and-Event-Introductiondoxcdocx/
+Powered by TCPDF (www.tcpdf.org)
