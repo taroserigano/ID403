a993def7ff26e0d0cacb67b3e73937f62973644a
Taro Serigano <taroserigano@gmail.com>
2022-11-03 07:19:59 -0500

    Create 2222.py

---
+++ 2222.py
@@ -0,0 +1,78 @@
+Running head: TECHNOLOGY AND EVENT INTRODUCTION 1
+Technology and Event of E-Filing
+Kadie Stokes
+SNHU
+IDS-403
+This study source was downloaded by 100000795539880 from CourseHero.com on 11-03-2022 07:16:00 GMT -05:00
+https://www.coursehero.com/file/169923707/IDS-403-Technology-and-Event-Short-paper-docx/
+TECHNOLOGY AND EVENT INTRODUCTION 2
+Technology and Event of E-Filing
+Over the years, technology has improved how tax preparers and people themselves can
+do their taxes more quickly and more error-free. From paper filing to electronic filing and up-todate technology that keeps all tax laws up to date for better tax reporting. This is the history of
+the start of electronic production and its process of improving how taxes are performed.
+Tax History
+Taxes began 4500 years ago in the early civilization in Mesopotamia where people use to
+pay taxes in the form of livestock. That was the preferred currency at the time. In 1913, the
+Sixteenth Amendment to the Constitution was ratified, giving Congress the power to apply
+personal income tax. In 1914, the Bureau of Internal Revenue issued the first income tax form
+called the 1040. This first year was a test for people to fill out the form and send it to the Bureau
+for verification without paying taxes. In 1916, the Income Tax Act started to adjust tax rates and
+income scales (IRS, 2022). At the beginning of 1980 was when the IRS began to see the
+implementation of new technology and began to use computers to process returns. The
+introduction of electronic filing was first introduced in 1986 (Davis, 2022).
+E-filing and Technology
+The history of electronic filing started in 1986, when five tax preparers from the
+Cincinnati, Raleigh-Durham and Phoenix metropolitan areas were offered to test the program.
+The electronic production process consists of using a machine known as Mitron (Davis, 2022).
+The machine worked when the tax preparer put a tape with tax information into the machine
+because the machine was a tape reader with a modem. This was done before transferring to the
+IRS. Once the tape was delivered to the IRS, it entered a machine called Zilog. This is a Zilog
+S8000 which was a small supercomputer that read the data on the tape and organized it in a
+manner that could be read by the IRS’s Unisys system (Davis, 2022). In 1988, IRS stopped
+operating the Zilog S8000 and started IBM Series 1 processing systems. During the 1990s and
+2000s, Telefile was used to file the 1040-ez form, which was completed by phone (Anderson,
+2005). In addition to all these ways of filing tax returns online, several different software
+programs have been put in place for tax preparers and individuals. Examples of software to use:
+Turbo Tax, Tax Act, H&R Block and Taxwise.
+Societal or Cultural influences
+This study source was downloaded by 100000795539880 from CourseHero.com on 11-03-2022 07:16:00 GMT -05:00
+https://www.coursehero.com/file/169923707/IDS-403-Technology-and-Event-Short-paper-docx/
+TECHNOLOGY AND EVENT INTRODUCTION 3
+As part of the IRS's efforts to promote e-filing systems, the IRS has partnered with a host
+of companies we have collectively referred to as the Free File alliance LLC. This offered free tax
+preparation for those accessing any of the businesses available on the IRS website (Fiorino,
+2005). We know that the use of tax software improves the experience of taxpayers and
+accelerates the processing of clients' taxes by tax preparers. Over the years, there has been
+evidence that people who use tax programs have eliminated the overpayment of taxes.
+Approximately 17.5% of paper income tax returns were the subject of an error that resulted in an
+overpayment of tax and only .5% of the electronic repositories had errors (Fiorino,2005). This
+process is faster with faster delivery of all refunds due to taxpayers. Not only has there been free
+filing offers to people, but there can be a small fee for filing the state filings. In the long run, it is
+worth it to a lot of people because of the savings it has made in reducing taxes. The only problem
+with e-filing is that it removes the social interaction that people would have with a tax preparer.
+As many people may use email and digitization technology to send the necessary data to a tax
+preparer to file tax returns for their clients. Even if just an individual does the taxes themselves
+there is no interaction unless they run into an issue, they can use the expert help through the
+software programs that is available to them.
+General education interdisciplinary lens
+I chose to write about the history of taxes and technology because I feel it is important
+that people understand the history of the origin of taxes. Taxes have existed for a long time and
+will not disappear any time soon. With the enhancement of technology every day, it helps make
+life easier, even when it comes to filing taxes. Technology takes many different forms and has
+made a difference for many businesses and individuals.
+This study source was downloaded by 100000795539880 from CourseHero.com on 11-03-2022 07:16:00 GMT -05:00
+https://www.coursehero.com/file/169923707/IDS-403-Technology-and-Event-Short-paper-docx/
+TECHNOLOGY AND EVENT INTRODUCTION 4
+References
+Anderson, T., Fox, M., Schwartz, B.N. (2005, Oct 1) History & Trends in E-filing: a Survey of
+CPA Practitioners. The CPA Journal 75 (10) http://search-ebscohostcom.ezproxy.snhu.edu/login.aspx?
+direct=true&db=edsbig&AN=edsbig.A138005068&site=eds-live&scope=site
+Davis, S. (2022, June 5) E-file history: Electronic tax filing in the United States. Efile.com
+ http://www.efile.com/efile-electronic-tax-filing-history/
+Fiorino, L. (2005, Feb 18) Web Sightings-IRS and Private companies partner for tax e-filing. The
+Daily Record (Baltimore, MD)
+IRS Website (2022, April 11) Income Tax History. Efile.com http://www.efile.com/tax-history-
+ and-the-tax-code
+This study source was downloaded by 100000795539880 from CourseHero.com on 11-03-2022 07:16:00 GMT -05:00
+https://www.coursehero.com/file/169923707/IDS-403-Technology-and-Event-Short-paper-docx/
+Powered by TCPDF (www.tcpdf.org)
