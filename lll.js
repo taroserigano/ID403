59d230d117d6ca5f3e8acdc26e8457964ffe64f8
Taro Serigano <taroserigano@gmail.com>
2022-11-03 07:03:02 -0500

    Update lll.js

--- lll.js
+++ lll.js
@@ -1,1 +1,52 @@
 hi
+
+Technology plays a huge role in my like. I feel that a big event in technology that
+I have experienced and still do is the ability to have internet to attend school
+and complete my projects for my job. Where I live and grew up, there has been
+no type of innovation to bring quality internet to my town. I’m 28 and this has
+been going on for year. The only thing available to my town in satellite internet
+which is like the bottom of the barrel. I found out that companies like Charter
+that offered cable internet had no internet in coming to my area. So after some
+heavy research, I started to find all types of alternatives but that weren’t exactly
+the right fit for the uses I needed it for. Luck would have it that out of the blue
+they placed a cell phone tower for ATT near my house and didn’t understand
+why in such a rural area. I immediately started researching again about the
+same internet source my phone uses which is LTE. Its fast, doesn’t go out in the
+rain and I can download and play games and shows without trouble. I
+unfortunately didn’t know that it could be used for a home internet source that
+was different in a sense from a mobile hotspot. After finding this out, I found a
+company, outside of ATT, that offered the same deal as if it were cable internet
+and have been in business ever since. The experience really opened my eyes on
+the beauty of technology. It taught me to never be stuck on normal solutions
+but to always go outside the box with other possibilities. Without that tower
+being near me, I don’t think I would have made in back into school.
+I would say that a current technological issue that I find interesting is the
+development of smart phones and how they seem stagnant. Cell phones have
+developed greatly over the years to where they are now. In the recent years
+though, I have found it interesting as to how a lot of companies, like Apple or
+Samsung, manage to make the same kind of phone. This is what I mean. For
+example, with Apple and Samsung, it has been a race with between these two
+titans. From the IPhone 7 and up you can see where the designs kind of start to
+become a bit sleeker and from there between the two companies it seems like it
+has be the same phone being developed over and over again. Minor differences
+you may see are differences in sizes (usually between 5 to 6 inches), the OS and
+an extra camera but other than that nothing to major that has changed. As a
+matter of fact, it seems with the last few phones the main obsession has been
+on the cameras. So much so to where they just come out with a new phone
+every year making their main concern about the camera. I have a Samsung Note
+10+ but I have it for the functionality purpose of lasting long, with a better
+battery. I have a sister that has the same phone but wants the Samsung Galaxy
+This study source was downloaded by 100000795539880 from CourseHero.com on 11-03-2022 07:02:28 GMT -05:00
+https://www.coursehero.com/file/68504763/2-1-Discussion-Technology-and-Youdocx/
+Note 20+ Ultra 5G mainly for the camera when both phone essentially work and
+operate the same. I suppose dealing with a lot of tech can make you aware of
+what makes a good phone and you start to notice a pattern when some
+companies manufacture the same phone each year with only one or two added
+feature but priced at about $1400. At a certain point, I’m hoping to see a
+smartphone with hologram features, ha ha.
+Savvides, Lexy. “Galaxy Note 20 Ultra vs. IPhone 11 Pro Max:
+Which Phone Has the Best Camera?” CNET, CNET, 7 Sept.
+2020, www.cnet.com/news/samsung-galaxy-note-20-ultra-andapple-iphone-11-pro-max-camera-comparison/.
+This study source was downloaded by 100000795539880 from CourseHero.com on 11-03-2022 07:02:28 GMT -05:00
+https://www.coursehero.com/file/68504763/2-1-Discussion-Technology-and-Youdocx/
+Powered by TCPDF (www.tcpdf.org)
